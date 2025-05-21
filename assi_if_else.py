119c2cc2e53c6fe1bd75b0644be1be9b7b1cf051
nagi11221 <merajahm1000@gmail.com>
2025-05-21 19:00:10 +0530

    this is new

---
+++ assi_if_else.py
@@ -0,0 +1,17 @@
+#you are creating  a simple program for "VENTEX HOTEL"
+#Theyserve indian dishes -idly dosa vadda ,upma, poori
+#every customer buys three items of any count
+#print final bill
+
+# what do you want in the output?
+# waht do you want in the output?
+# pseudo Codee 
+# implement
+
+menu = {
+    'Burgir':240,
+    'goat burgir':300,
+    'Bade ka burgir':400,
+    'fish burgir':350,
+}
+print(menu)
